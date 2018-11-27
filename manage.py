f63f80d2062a1a940d540d100fb59de19b1c4b89
tiffanymugure <amiratiffany@gmail.com>
2018-11-27 10:26:01 +0300

    files updated

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from app.models import User, Comments, Post, Subscriber
 # app = create_app('test')
 
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server', Server)
